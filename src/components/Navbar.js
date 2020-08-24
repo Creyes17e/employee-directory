3b19b75da0ea0ba67916ad2e30328f3d25d20a43
Carina Reyes <cereyes1792@gmail.com>
2020-08-24 18:31:36 -0500

    styled nav added search file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,15 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="jumbotron text-center">
+    <div
+      className="jumbotron text-center"
+      style={{
+        color: "white",
+        backgroundColor: "#052675",
+        borderBottom: "solid",
+        borderBottomColor: "red",
+      }}
+    >
       <h1>Employee Directory</h1>
     </div>
   );

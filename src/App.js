d296ac51c0533d84914db2714cf41db09476b04f
Carina Reyes <cereyes1792@gmail.com>
2020-08-24 23:51:29 -0500

    added a searchresults component, created table

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "../src/components/Navbar";
 import SearchForm from "../src/components/SearchForm";
+import Search from "../src/components/Search";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <SearchForm />
+      <Search />
     </Router>
   );
 }
---
+++ src/components/SearchResults.js
@@ -0,0 +1,18 @@
+import react from "react";
+
+function SearchResults(props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>Email</th>
+          <th>DOB</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  );
+}
+export default SearchResults;

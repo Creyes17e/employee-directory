b1b1329e0b62f675ee36a95d99ddaea6f4183404
Carina Reyes <cereyes1792@gmail.com>
2020-08-25 22:24:21 -0500

    install react moment to format dob centered text

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,17 +1,19 @@
 import React from "react";
+import Moment from "react-moment";
 
 function SearchResults(props) {
   return (
     <table>
       <thead>
-        <tr>
+        <tr className="text-center">
+          <th>Image</th>
           <th>Name</th>
           <th>Phone</th>
           <th>Email</th>
           <th>DOB</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className="text-center">
         {props.results.map((result) => (
           <tr className="table" key={result.login.uuid}>
             <td>
@@ -21,10 +23,12 @@ function SearchResults(props) {
                 alt="employee-selfie"
               />
             </td>
-            <td>{result.name.first + "" + result.name.last}</td>;
+            <td>{result.name.first + " " + result.name.last}</td>
             <td>{result.cell}</td>
             <td>{result.email}</td>
-            <td>{result.dob.date}</td>
+            <td>
+              <Moment format="MM/DD/YYYY">{result.dob.date}</Moment>
+            </td>
           </tr>
         ))}
       </tbody>
